06efea87f155e70778c729f17343938a73466792
Christian Verollet <verollet.christian@gmail.com>
2026-09-11 11:35:26 +0200

    v0.1.1

--- __init__.py
+++ __init__.py
@@ -13,6 +13,10 @@ from .base import PVFunction
 
 from .functions import A0, B0, A00, B1, B11, B00, dB0_dp2, dB1_dp2, dB11_dp2, dB00_dp2
 
+from .symbols import m, m_0, m_1, m_2, m_3, m_4, k, p, p_1, p_2, p_3, p_4, k2, p2, p_12, p_22, p_32, p_42,mu, nu, rho, sigma 
+
+from .numeric import compile
+
 __all__ = [
     # algebra
     "g", "Mom", "Dot", "Eps", "contract",
